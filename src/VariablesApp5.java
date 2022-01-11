2b50818f1b506069bcd7f65a8631457b80a1e57e
Raúl <rgarridorasillo@gmail.com>
2022-01-11 20:49:30 +0100

    TAE4 4

---
+++ src/VariablesApp5.java
@@ -0,0 +1,23 @@
+
+public class VariablesApp5 {
+
+	public static void main(String[] args) {
+		
+		int n = 3;
+		
+		System.out.println("Valor inicial de N = " + n);
+		
+		n+=77;
+		
+		System.out.println("N + 77 = " + n);
+		
+		n-=3;
+		
+		System.out.println("N - 3 = " + n);
+		
+		n*=2;
+		
+		System.out.println("N * 2 = " + n);
+	}
+
+}
